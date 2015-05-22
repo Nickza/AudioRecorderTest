95272317c382a51a4c416e3c4c4c0c3efb257fdd
Nickza <nick.orpen@gmail.com>
2015-05-22 13:45:24 +0200

    fri12

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,6 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/com.nickza.audiorecorder/www/audiorecorder.js",
+        "id": "com.nickza.audiorecorder.AudioRecorder",
+        "clobbers": [
+            "AudioRecorder"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
@@ -9,6 +16,7 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "com.nickza.audiorecorder": "0.0.1",
     "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
